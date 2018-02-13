10b05d8e75cc545bdb434c0b1807bf0b19f068cc
FerVilleda <fer_mcr_95@hotmail.com>
2018-02-13 15:52:20 -0600

    Funcionamiento
    Se define el funcionamiento de las funciones en comentarios

--- cachematrix.R
+++ cachematrix.R
@@ -4,14 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x=matrix()) {
+  ##la inversa es nula al iniciar la funcion
   inv <- NULL
+  ##Envia el valor y como x para la matriz
   set <- function(y){
     x <<- y
     inv <<- NULL
   }
+  ##obtiene el valor de la matriz
   get <- function() x
+  ##Obtiene y envia la inversa de la matriz
   setinv <- function(inverse) inv <<- inverse
   getinv <- function() inv
+  ##elementos que se pueden consultar al utilizar la funcion
   list(set = set, get = get,
        setinv = setinv,
        getinv = getinv)
@@ -22,14 +27,19 @@ makeCacheMatrix <- function(x=matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ##Se revisa si hay valor de inversa si no se calcula
   inv <- x$getinv()
   if(!is.null(inv)){
+    ##Se regresan los datos almacenados
     message("Obteniendo los datos almacenados")
     return(inv)
   }
+  ##Se calcula la inversa
   mat.data = x$get()
   inv = solve(mat.data, ...)
+  ##Se envia para almacenar en cache
   x$setinv(inv)
+  ##Se regresa el valor calculado
   return(inv)
 }
 
